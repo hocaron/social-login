40cea131a3dc971abc807172c8a270e1bd5cc3dd
haron47 <pooh0407@ewhain.net>
2021-12-16 17:17:02 +0900

    feat :: create google auth guard

--- src/auth/guard/google-auth.guard.ts
+++ src/auth/guard/google-auth.guard.ts
@@ -2,4 +2,4 @@ import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class KakaoAuthGuard extends AuthGuard('kakao') {}
+export class GoogleAuthGuard extends AuthGuard('google') {}
