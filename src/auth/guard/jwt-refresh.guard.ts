aae172c745e831750618284a6439f4a728eb8342
haron47 <pooh0407@ewhain.net>
2021-12-19 23:58:42 +0900

    feat :: refresh strtegy 생성

--- src/auth/guard/jwt-refresh.guard.ts
+++ src/auth/guard/jwt-refresh.guard.ts
@@ -1,5 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import * as CryptoJS from 'crypto-js';
+import { AuthService } from '../auth.service';
+import { Err } from './../../error';
+import { UserService } from './../../user/user.service';
 
 @Injectable()
-export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token') {}
+export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token') {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+
+    const { authorization } = request.headers;
+    if (authorization === undefined) {
+      throw new BadRequestException(Err.TOKEN.NOT_SEND_REFRESH_TOKEN);
+    }
+
+    const refreshToken = authorization.replace('Bearer ', '');
+    const refreshTokenValidate = await this.validate(refreshToken);
+    request.user = refreshTokenValidate;
+    return true;
+  }
+
+  async validate(refreshToken: string) {
+    try {
+      const bytes = CryptoJS.AES.decrypt(refreshToken, process.env.AES_KEY);
+      const token = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+      const tokenVerify = await this.authService.tokenValidate(token);
+      const user = await this.userService.findUserById(tokenVerify.id);
+      if (user.refreshToken === refreshToken) {
+        return user;
+      } else {
+        throw new Error('no permission');
+      }
+    } catch (error) {
+      switch (error.message) {
+        // 토큰에 대한 오류를 판단합니다.
+        case 'invalid token':
+          throw new BadRequestException(Err.TOKEN.INVALID_TOKEN);
+
+        case 'no permission':
+          throw new BadRequestException(Err.TOKEN.NO_PERMISSION);
+
+        case 'jwt expired':
+          throw new BadRequestException(Err.TOKEN.JWT_EXPIRED);
+
+        default:
+          throw new HttpException('서버 오류입니다.', 500);
+      }
+    }
+  }
+}
---
+++ src/auth/strategy/jwt-refresh.strategy.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtRefreshStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh-token',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+}
